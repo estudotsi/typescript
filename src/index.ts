0e50a65af3d82b4f7a1f1e44fa2cfe3e27ebb8cd
Rogerio Martins Campos Martins Campos <zzmelzz@hotmail.com>
2023-04-26 12:22:02 -0300

    2604

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-let nome: string = 'Rogerio';
+/*let nome: string = 'Rogerio';
 let ano: number = 21;
 
 function retornaNome(): string{
@@ -15,3 +15,25 @@ function multiplicar(a: number, b: number): number{
     return a * b; 
 }
 console.log(multiplicar(5,5));
+
+
+let calculo;
+calculo = digaOi;
+calculo();
+
+calculo = multiplicar;
+console.log(calculo(6,6));*/
+
+let funcionario: { supervisores: string[], baterPonto: (horas: number) => string } = {supervisores: ['Ana', 'Fernado'], baterPonto(horario: number): string {
+    if(horario <= 8){
+        return 'Ponto normal'
+    }else{
+        return 'Fora do Horário'
+    }
+}}
+
+console.log(funcionario.supervisores);
+console.log(funcionario.baterPonto(8));
+console.log(funcionario.baterPonto(9));
+
+
